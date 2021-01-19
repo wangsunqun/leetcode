725080d0e8cebd7236e1411bfc925f46fa8d10ad
S4545 <wangsunqun@corp.netease.com>
2021-01-19 11:46:33 +0800

    1480

---
+++ src/main/java/com/wsq/leetcode/easy/Solution.java
@@ -0,0 +1,25 @@
+package com.wsq.leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * 1480
+ *
+ * @author wsq
+ * 2021/1/19 11:32
+ */
+public class Solution {
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+}
